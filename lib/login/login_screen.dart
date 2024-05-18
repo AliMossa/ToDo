e666d5f40b1488f574e8c1c70b333f14a1044cbb
AliMossa <131074478+AliMossa@users.noreply.github.com>
2024-05-19 02:03:12 +0300

    Update login_screen.dart

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo/home/bloc/home_bloc.dart';
 import 'package:todo/login/bloc/login_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
-  final _formKey = GlobalKey<FormState>();
-   LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +23,8 @@ class LoginScreen extends StatelessWidget {
                     ),
                     borderRadius: BorderRadius.circular(20)),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your username.';
-                    }
-                    return null;
-                  },
                   onChanged: (value) {
-                    print("the value is $value");
+                    print("the valeu is $value");
                     context.read<LoginBloc>().data.userNameV = value;
                   },
                   onFieldSubmitted: (_) {
@@ -53,17 +45,11 @@ class LoginScreen extends StatelessWidget {
                     ),
                     borderRadius: BorderRadius.circular(20)),
                 child: TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password.';
-                    }
-                    return null;
-                  },
                   onChanged: (value) {
                     context.read<LoginBloc>().data.passwordV = value;
                   },
                   focusNode: context.watch<LoginBloc>().data.password,
-                  obscureText:!context.watch<LoginBloc>().data.secure,
+                  obscureText: !context.watch<LoginBloc>().data.secure,
                   decoration: InputDecoration(
                       suffixIcon: InkWell(
                         onTap: () {
@@ -78,15 +64,47 @@ class LoginScreen extends StatelessWidget {
                       border: InputBorder.none),
                 ),
               ),
-              // Add a button to trigger validation
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // If all forms are valid, proceed with login logic
-                    context.read<LoginBloc>().add(SendLoginEvent());
+              BlocConsumer<LoginBloc, LoginState>(
+                listener: (context, state) {
+                  print(state);
+                  if (state is ExceptionLoginState) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text(
+                        "Login has been failed",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      backgroundColor: Colors.red,
+                      behavior: SnackBarBehavior.floating,
+                    ));
+                  } else if (state is SuccessLoginState) {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/', (route) => false);
                   }
                 },
-                child: const Text('Submit'),
+                builder: (context, state) => SizedBox(
+                  width: moreInfo.width * 0.8,
+                  height: moreInfo.height * .08,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.blue.withOpacity(.8),
+                      ),
+                    ),
+                    onPressed: () {
+                      context.read<LoginBloc>().add(SendLoginEvent());
+                    },
+                    child: context.watch<LoginBloc>().state is LoadingLoginState
+                        ? const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
+                    )
+                        : const Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
